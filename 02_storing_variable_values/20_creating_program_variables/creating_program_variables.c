4110befba71b9baca7247630be8110a8f914a268
SSR <iamssrobinson@gmail.com>
2023-01-02 03:27:01 +0000

    Practice creating variables of differing data-types.

---
+++ 02_storing_variable_values/20_creating_program_variables/creating_program_variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+	int num1, num2; // declares 2 variables of int data-type
+	char letter; // declares a variable of char data-type
+	float decimal = 7.5; // declares and initializes a variable of float data-type and assigns value to it
+	num1 = 100; // initializes num1 variable with an integer value
+  letter = 'A'; // initializes letter variable with a character value
+
+	printf("%d\n", num1);
+	 return 0;
+}
