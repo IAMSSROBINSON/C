a0123830cb59b0667cb42f0180bb6e39c6213e6d
SSR <iamssrobinson@gmail.com>
2023-01-05 05:13:49 +0000

    Added comments for printf results. Next: check long int results?

--- 02_storing_variable_values/26_qualifying_data_types/sizeof.c
+++ 02_storing_variable_values/26_qualifying_data_types/sizeof.c
@@ -9,12 +9,12 @@ int main () {
 
 	// output the size and range of long int data-type:
 	printf("long int... \t size: %d bytes \t", sizeof( long int));
-	printf("%ld to %ld \n", LONG_MAX, LONG_MIN);
+	printf("%ld to %ld \n", LONG_MAX, LONG_MIN); // long int...      size: 8 bytes  9223372036854775807 to -9223372036854775808 
 
 	// output the sizeof other data-types:
-	printf("char... \tsize: %d byte \t \n", sizeof( char ));
-	printf("float... \tsize: %d bytes \t \n", sizeof( float ));
-	printf("double... \tsize: %d bytes \t \n", sizeof( double ));
+	printf("char... \tsize: %d byte \t \n", sizeof( char )); // char...         size: 1 byte  
+	printf("float... \tsize: %d bytes \t \n", sizeof( float )); // float...        size: 4 bytes  
+	printf("double... \tsize: %d bytes \t \n", sizeof( double )); // double...       size: 8 bytes  
 
 	return 0;
 }
