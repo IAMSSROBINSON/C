8a1a2a7e8598791def8b631c112ac17589115848
SSR <iamssrobinson@gmail.com>
2023-01-14 05:16:52 +0000

    Added macro to identify Apple operating system.

--- 03_setting_constant_values/46_defining_constants/define.c
+++ 03_setting_constant_values/46_defining_constants/define.c
@@ -13,11 +13,17 @@
 #define SYSTEM "Linux"
 #endif
 
+// macro: to identify Apple host platform
+#ifdef __APPLE__
+#define SYSTEM "Apple"
+#endif
+
 
 int main () {
 	// print the string-text substituted by the preprocessor
 	printf("\n\n\t%s\n\t\t%s\t\n\t%s\n",LINE, TITLE, LINE);
 	printf("\t%s\t\n\n\n", AUTHOR);
+	printf("\n\t\t\tOS:%s\n\n", SYSTEM);
 
 	return 0;
 }
