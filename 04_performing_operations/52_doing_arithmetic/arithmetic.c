4e0b915ce8207174cfe93bb6e9ca0efeb9a4bc2a
SSR <iamssrobinson@gmail.com>
2023-01-16 03:04:15 +0000

    Practiced using arithmetic operators, performing operations and displaying results. Prefix and postfix operators also demonstrated.

--- 04_performing_operations/52_doing_arithmetic/arithmetic.c
+++ 04_performing_operations/52_doing_arithmetic/arithmetic.c
@@ -6,10 +6,17 @@ int main () {
 	int a = 5, b = 10, c = 15, d = 20;
 
 	// print the result of arithmetic operations performed on the numbers:
-	printf("Addition: %d\n", a + b); // 5 + 10 = 15
+	printf("\nAddition: %d\n", a + b); // 5 + 10 = 15
 	printf("Subtraction: %d\n", b - a); // 10 - 5 = 5
 	printf("Multiplication: %d\n", a * b); // 5 * 10 = 50
 	printf("Division: %d\n", b / a); // 10 * 5 = 2
-	printf("Modulus: %d\n", a % b); // 5 % 10 = 5
+	printf("Modulus: %d\n\n", a % b); // 5 % 10 = 5
+
+	// show the result of using prefix and postfix operators
+	printf("\nPrefix increment: %d\n", ++d); // 21
+	printf("\nPrefix now: %d\n", d); // 21
+	printf("\nPostfix increment: %d\n",c++); // 15
+	printf("\nPostfix now: %d\n", c); // 16
+	
 	return 0;
 }
