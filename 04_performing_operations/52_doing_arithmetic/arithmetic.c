6fc5fab68cbdf0902a5ee08f93980f9691683a14
SSR <iamssrobinson@gmail.com>
2023-01-16 02:57:28 +0000

    Performed and printed the results of some arithmetic operations using arithmetic operators.

---
+++ 04_performing_operations/52_doing_arithmetic/arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+	
+	// declare and initialize some integer variables
+	int a = 5, b = 10, c = 15, d = 20;
+
+	// print the result of arithmetic operations performed on the numbers:
+	printf("Addition: %d\n", a + b); // 5 + 10 = 15
+	printf("Subtraction: %d\n", b - a); // 10 - 5 = 5
+	printf("Multiplication: %d\n", a * b); // 5 * 10 = 50
+	printf("Division: %d\n", b / a); // 10 * 5 = 2
+	printf("Modulus: %d\n", a % b); // 5 % 10 = 5
+	return 0;
+}
