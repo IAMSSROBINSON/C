54ba32b5f2b3458f8898fdc305e3cd58d9b5226f
SSR <iamssrobinson@gmail.com>
2023-01-20 03:10:03 +0000

    Practiced using ternary operator to determine the correct grammar to use depenent on conditional result.

--- 04_performing_operations/60_examining_conditions/conditional.c
+++ 04_performing_operations/60_examining_conditions/conditional.c
@@ -3,10 +3,15 @@
 int main () {
 
 	// declare and initialize int variable
-	int number = 7;
+	int number = 100;
 
 	// use ternary to output correct result parity dependant
-	printf("The number: %d is: %s\n",number, (number % 2 == 0) ? "Even" : "Odd");
+	printf("The number: %d is: %s\n",number, (number % 2 == 0) ? "Even" : "Odd"); // The number: 100 is: Even
+
+
+	// output correct phrase dependent on result
+	printf("There %s ", (number == 1) ? "is" : "are"); // There are
+	printf("%d %s\n", number, (number == 1) ? "apple" : "apples"); // 100 apples
 
 	return 0;
 }
