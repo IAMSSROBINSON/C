2fcff5fbd8fc79ec7bad174fdd2d3e9baf115df3
SSR <iamssrobinson@gmail.com>
2023-02-03 03:56:33 +0000

    Used 'continue'statement to skip iteration of a nested loop.

--- 05_making_statements/80_breaking_out_of_loops/breakcontinue.c
+++ 05_making_statements/80_breaking_out_of_loops/breakcontinue.c
@@ -10,6 +10,10 @@ int main () {
 	
 	for(int i = 1; i < 4; i++){
 		for(int j = 1; j < 4; j++){
+			if(i == 1 && j == 1){
+				printf("Continues inner loop when i = %d and j = %d\n", i, j);
+				continue;
+			}
 			if(i == 2 && j == 1){
 				printf("Break inner loop when i = %d and j = %d\n", i, j);
 				break;
