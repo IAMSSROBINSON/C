7970f44041f137cce77191ff0475f15f55c67189
SSR <iamssrobinson@gmail.com>
2023-02-03 03:47:59 +0000

    Practiced breaking out of inner loop.

--- 05_making_statements/80_breaking_out_of_loops/breakcontinue.c
+++ 05_making_statements/80_breaking_out_of_loops/breakcontinue.c
@@ -7,8 +7,13 @@ int main () {
 
 	// use two nested loops to display their counter values on each of their 3 iterations
 
+	
 	for(int i = 1; i < 4; i++){
 		for(int j = 1; j < 4; j++){
+			if(i == 2 && j == 1){
+				printf("Break inner loop when i = %d and j = %d\n", i, j);
+				break;
+			}
 			printf("Running I =  %d J = %d\n",i, j);
 		}
 	}
