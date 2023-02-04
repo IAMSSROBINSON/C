9d1ce56c62895d9402a979e5bf23edd38fe40c23
SSR <iamssrobinson@gmail.com>
2023-02-04 05:48:37 +0000

    Practiced using goto statement with if conditional expression to jump from within a nested loop goto labelName to outside the loop at the labelName: terminating the loop when the condition was met.

---
+++ 05_making_statements/82_going_to_labels/jump.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+
+	// declare 2 int variables
+	int i, j;
+
+	// use 2 nested loops to display the index values
+	// add goto statement within loop to jump outside 
+	for(int i = 0; i < 4; i++){
+		for(int j = 0; j < 4; j++){
+			if(i == 2 && j == 1){
+				goto end;
+			}
+			printf("I = %d J = %d\n", i, j);
+	}
+	} end:
+
+	return 0;
+}
