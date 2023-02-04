ab00f437068a187ca3b40a1feb0d234a28c3f52c
SSR <iamssrobinson@gmail.com>
2023-02-04 06:18:57 +0000

    cd

--- 05_making_statements/82_going_to_labels/jump.c
+++ 05_making_statements/82_going_to_labels/jump.c
@@ -4,6 +4,7 @@ int main () {
 
 	// declare 2 int variables
 	int i, j;
+	int flag = 1;
 
 	// use 2 nested loops to display the index values
 	// add goto statement within loop to jump outside 
@@ -12,9 +13,27 @@ int main () {
 			if(i == 2 && j == 1){
 				goto end;
 			}
-			printf("I = %d J = %d\n", i, j);
-	}
+		printf("I = %d J = %d\n", i, j);
+		}
 	} end:
+	
+
+	// int flag = 1 variable added above to represent boolean true value
+	for(int i = 1; i < 4; i++){
+		if(flag){
+			for(int j = 1; j < 4; j++){
+				if(i == 2 && j == 1){
+					flag = 0;
+				}
+			if(flag){
+				printf("\tI = %d J = %d\n", i, j);
+			}
+			}
+		}
+	} 
+	
+
 
 	return 0;
 }
+
