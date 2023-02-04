8fd062e8cc596cc4abb948fe4a470f82be523032
SSR <iamssrobinson@gmail.com>
2023-02-04 05:50:23 +0000

    Amended index value to make output more succint.

--- 05_making_statements/82_going_to_labels/jump.c
+++ 05_making_statements/82_going_to_labels/jump.c
@@ -7,8 +7,8 @@ int main () {
 
 	// use 2 nested loops to display the index values
 	// add goto statement within loop to jump outside 
-	for(int i = 0; i < 4; i++){
-		for(int j = 0; j < 4; j++){
+	for(int i = 1; i < 4; i++){
+		for(int j = 1; j < 4; j++){
 			if(i == 2 && j == 1){
 				goto end;
 			}
