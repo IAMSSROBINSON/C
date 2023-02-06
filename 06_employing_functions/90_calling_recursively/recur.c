5847418158aa2ec4633d3c0c34c20c4df255d2b5
SSR <iamssrobinson@gmail.com>
2023-02-06 02:14:42 +0000

    Practiced working with recursive functions.

---
+++ 06_employing_functions/90_calling_recursively/recur.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+// declare custom function prototype
+void counting_down_from (int startingNumber);
+
+int main () {
+
+	// declare int variable 
+	int startingNumber;
+
+	// prompt user for input
+	printf("Please enter a whole number: ");
+	scanf("%d", &startingNumber);
+	printf("\n");
+
+	// call custom function and pass int variable
+	counting_down_from (startingNumber);
+
+	// display message when program resumes from custom function call
+	printf("We have lift off!\n");
+
+	return 0;
+}
+
+// define custom function
+// display value
+// modify value / decrement value
+// add base case condition
+void counting_down_from (int startingNumber) {
+	printf("%d\n", startingNumber);
+	startingNumber--;
+	if(startingNumber < 0){
+		return;
+	} else {
+		counting_down_from(startingNumber);
+	}
+}
+
+/*
+
+DISPLAYED OUTPUT:
+
+Please enter a whole number: 10
+
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+We have lift off!
+
+*/
