3fcf0b6d92aa7465312ba9fcb7aa2eeedabc4bdb
SSR <iamssrobinson@gmail.com>
2023-02-14 02:29:02 +0000

    Used itoa() and sprintf() functions both accepting 3arguments to convert int values to alpha values using differing format specifiers and base conversion values.

--- 08_manipulating_strings/120_converting_strings/conv.c
+++ 08_manipulating_strings/120_converting_strings/conv.c
@@ -21,6 +21,24 @@ int main () {
 	printf("String: %s converts to Integer: %d \n", string3, number3);
 	// String: x13 converts to Integer: 0 
 
+	// convert int variable to string binary equivalent base of 2 and display
+	itoa(number1, string2, 2);
+	printf("Decimal: %d is Binary: %s\n", number1, string1);
+	// Decimal: 12 is Binary: 1100
+
+	// convert int variable value to string containing octal and hexidecimal equivalents and store char length
+	number2 = sprintf(string3, "%o", number1);
+	printf("Decimal: %d is Octal: %s chars: %d\n", number1, string3, number2);
+	//Decimal: 12 is Octal: 14 chars: 2
+
+	number3 = sprintf(string3, "%x", number1);
+	printf("Decimal: %d is Hexadecimal: %s chars: %d\n", number1, string3, number3);
+	// Decimal: 12 is Hexadecimal: c chars: 1
+
+
+
+
+
 
 	return 0;
 }
