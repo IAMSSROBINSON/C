6ce77b7b3f59d6aed73b9664b59bc726fc539a90
SSR <iamssrobinson@gmail.com>
2023-02-24 03:01:43 +0000

    used strcpy to assign new string to char array, used fputs(); to write the char array to file_ptr and used fclose(); to close file_ptr.

--- 10_producing_results/146_reading_and_writing_lines/lines.c
+++ 10_producing_results/146_reading_and_writing_lines/lines.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main () {
 
 	// declare file ptr and char array
-
 	FILE *file_ptr;
 	char sentence[50];
 
@@ -15,11 +16,35 @@ int main () {
 		while(fgets(sentence, 50, file_ptr)){
 			printf("%s", sentence);
 		}
+
+	// copy new string to char array
+	strcpy(sentence, "...By Marcus Aurelius");
+	fputs(sentence, file_ptr);
+
+	// close the opened file
+	fclose(file_ptr);
+	return 0;
 	}
-
-
-
+	else {
+		// failure message
+		printf("Unable to open file\n");
+		return 1;
+	}
 
 
 	return 0;
 }
+
+
+/*
+
+DISPLAYED OUTPUT - BEFORE APPENDING:
+
+“Dwell on the beauty of life. Watch the stars, and see yourself running with them. Think constantly on the changes of the elements into each other, for such thoughts wash away the dust of earthly life.”
+
+
+DISPLAYED OUTPUT - AFTER APPENDING:
+
+“Dwell on the beauty of life. Watch the stars, and see yourself running with them. Think constantly on the changes of the elements into each other, for such thoughts wash away the dust of earthly life.”...By Marcus Aurelius
+
+*/
