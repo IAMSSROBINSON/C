55ce530f6435a078278c131610f7f559f6bef004
SSR <iamssrobinson@gmail.com>
2023-02-25 05:08:01 +0000

    Practiced reading and writing entire files. **Look into why extra text printed at the end of the file? Might need to clean the memory for that ptr first before writing to it?**

---
+++ 10_producing_results/148_reading_and_writing_entire_files/readwrite.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main () {
+
+	// declare two file pointers, char array variable and int variable
+	FILE *original_ptr;
+	FILE *copy_ptr;
+
+	char buffer[1000];
+	int number;
+
+	// open existing file to read
+	// open/create a file to write
+	original_ptr = fopen("./original.txt", "r");
+	copy_ptr = fopen("./copy.txt", "w");
+
+	// validate files were opened
+	// read original file contents to char array, count each object read
+	// write array content to second file
+	if((original_ptr != NULL) && (copy_ptr != NULL)){
+		number = fread(buffer, 1, 1000, original_ptr);
+		fwrite(buffer, 1, 1000, copy_ptr);
+
+		// close files when complete
+		fclose(original_ptr);
+		fclose(copy_ptr);
+
+		// print confirmation message
+		printf("Done: original.txt copied to copy.txt\n");
+		printf("%d objects copied\n", number);
+		return 0;
+	} 
+	// failed message
+	else{
+		if(original_ptr == NULL){
+			printf("Unable to open original.txt");
+		}
+			if(copy_ptr == NULL){
+			printf("Unable to open copy.txt");
+		}
+		return 1;
+	}
+
+	return 0;
+}
